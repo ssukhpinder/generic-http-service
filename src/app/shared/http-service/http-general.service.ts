fef251dbd895de8547492e508d60c6c799c77744
Sukhpinder Singh <66723473+sukhsukhpinder@users.noreply.github.com>
2020-07-22 12:25:25 +0530

    added common header function

--- src/app/shared/http-service/http-general.service.ts
+++ src/app/shared/http-service/http-general.service.ts
@@ -11,19 +11,23 @@ export class HttpGeneralService {
     this.url = environment.apiRoute;
   }
 
-  post(apiRoute: string, body: any, header?: HttpHeaders) {
-    return this.http.post(`${this.url + apiRoute}`, body, { headers: header });
+  post(apiRoute: string, body: any) {
+    return this.http.post(`${this.url + apiRoute}`, body, { headers: this.getHttpHeaders() });
   }
 
-  get(apiRoute: string, header?: HttpHeaders) {
-    return this.http.get(`${this.url + apiRoute}`, { headers: header });
+  get(apiRoute: string) {
+    return this.http.get(`${this.url + apiRoute}`, { headers: this.getHttpHeaders() });
   }
 
-  put(apiRoute: string, body: any, header?: HttpHeaders) {
-    return this.http.put(`${this.url + apiRoute}`, body, { headers: header });
+  put(apiRoute: string, body: any) {
+    return this.http.put(`${this.url + apiRoute}`, body, { headers: this.getHttpHeaders() });
   }
 
-  delete(apiRoute: string, header?: HttpHeaders) {
-    return this.http.delete(`${this.url + apiRoute}`, { headers: header });
+  delete(apiRoute: string) {
+    return this.http.delete(`${this.url + apiRoute}`, { headers: this.getHttpHeaders() });
+  }
+
+  getHttpHeaders(): HttpHeaders {
+    return new HttpHeaders().set("key", "value");
   }
 }
